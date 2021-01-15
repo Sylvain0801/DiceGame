788d0c7b49282d161a1d4de120223ce48abc1e95
sylvain0801 <treillardsylvain@hotmail.fr>
2021-01-15 23:13:08 +0100

    Ajout fonctionnalité new game

--- app.js
+++ app.js
@@ -8,17 +8,30 @@ const point = document.querySelectorAll('.point')
 
 const allFace = [f1, f2, f3, f4, f5, f6]
 
+const player1 = document.getElementById('player-1')
+const player2 = document.getElementById('player-2')
+
+
+const current1 = document.getElementById('current-1')
+const current2 = document.getElementById('current-2')
+const current = [current1, current2]
+const global1 = document.getElementById('global-1')
+const global2 = document.getElementById('global-2')
+
+var activePlayer = 1
+var isPlaying = false
+
 // gestion button roll dice
 const rollDice = document.getElementById('roll-dice')
 const dice = document.querySelector('.dice')
 
-const delay = 250
+const delay = 150
 
 var animation
 
-var no = 0
+var no = 1
 
-const duration = Math.floor(Math.random() * 3000 + 1000)
+const duration = Math.floor(Math.random() * 1500 + 500)
 
 const launch = () => {
   setTimeout((delay) => removeActive(), delay / 3)
@@ -28,7 +41,6 @@ const launch = () => {
 const randomDice = () => {
   no = Math.floor(Math.random() * 6)
   displayDice(no)
-  return no
 }
 
 const displayDice = (no) => {
@@ -42,23 +54,56 @@ const removeActive = () => {
   point.forEach(item => item.classList.remove('active'))
 }
 
-rollDice.addEventListener('click', (e) => {
-  e.stopPropagation()
+const newDice = () => {
   animation = setInterval((delay) => launch(), delay)
   dice.style.animation = `shake .1s ease-in-out ${duration / 100} backwards`
   setTimeout(() => {
-    clearInterval(animation),
-    setTimeout( delay => displayDice(no), delay),
-    dice.style.animation = 'none',
+    clearInterval(animation)
+    setTimeout( delay => displayDice(no), delay)
+    dice.style.animation = 'none'
     animation = null
+    if (isPlaying) {
+      current[activePlayer].textContent = formatNumberTwoDigits(parseInt(current[activePlayer].textContent) + no + 1)
+    }
+    return no
   }, duration)
+}
+
+const formatNumberTwoDigits = value => {
+  value = value.toString()
+  if (value.length === 1) {
+    value = '0' + value
+  }
+  return value
+}
+
+rollDice.addEventListener('click', (e) => {
+  newDice()
 })
 
-randomDice()
-
+const whoStartPlaying = (no) => {
+  no % 2 === 0 ? (
+    console.log(typeof no),
+    activePlayer = 1,
+    player2.classList.add('active-player'),
+    player1.classList.remove('active-player')
+    ) : (
+    activePlayer = 0,
+    player1.classList.add('active-player'),
+    player2.classList.remove('active-player')
+    )
+  isPlaying = true
+}
 // gestion button new game
 const newGame = document.getElementById('new-game')
 
-newGame.addEventListener('click', () => {
-  randomDice()
+newGame.addEventListener('click', (e) => {
+  current1.textContent = current2.textContent = '00'
+  global1.textContent = global2.textContent = '00'
+  isPlaying = false
+  newDice()
+  setTimeout(duration => whoStartPlaying(no), duration)
 })
+
+newDice()
+setTimeout(duration => whoStartPlaying(no), duration)
